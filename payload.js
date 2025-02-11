662fc24f448776c49272ab5835ac2a90749dea77
ramzisx18 <121553217+ramzisx18@users.noreply.github.com>
2025-02-11 01:03:00 +0100

    Update payload.js

--- payload.js
+++ payload.js
@@ -7,13 +7,12 @@ document.body.style.height = '100vh';
 document.body.style.overflow = 'hidden';
 
 document.addEventListener("DOMContentLoaded", function() {
-    let video = document.createElement('video');
-    video.src = 'video.mp4'; // استبدل باسم ملف الفيديو الخاص بك
-    video.autoplay = true;
-    video.loop = true;
-    video.controls = false;
-    video.style.maxWidth = '100%';
-    video.style.maxHeight = '100%';
+    let iframe = document.createElement('iframe');
+    iframe.src = 'https://www.youtube.com/embed/mUb4iwrcq8U?autoplay=1&loop=1&playlist=mUb4iwrcq8U';
+    iframe.allow = 'autoplay';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.border = 'none';
     
-    document.body.appendChild(video);
+    document.body.appendChild(iframe);
 });
